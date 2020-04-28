26541434a1d7fdce240ba08fb0f1e6d08f01e5db
Wilfred Tyler Gee <wtylergee@gmail.com>
2020-04-28 06:48:37 -1000

    Cleaning up dependenies and simplifying. Removing unused and adding panoptes-utils minimum.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 
 from setuptools import setup, find_namespace_packages
 
-import itertools
-
 from configparser import ConfigParser
 from distutils.command.build_py import build_py
 
@@ -24,50 +22,31 @@ LONG_DESCRIPTION = metadata.get('long_description', '')
 PACKAGENAME = metadata.get('package_name', 'packagename')
 URL = metadata.get('url', 'http://projectpanoptes.org')
 
-modules = {
-    'google': [
-        'gcloud',
-        'google-cloud-storage',
-    ],
-    'mongo': [
-        'pymongo>=3.2.2',
-    ],
-    'dev': [
-        'jupyter-console',
-        'jupyterlab',
-    ],
-    'required': [
-        'astroplan>=0.6',
-        'astropy>=4.0.0',
-        'dateparser',
-        'matplotlib',
-        'numpy',
-        'pandas',
-        'panoptes-utils',
-        'photutils',
-        'pyserial>=3.1.1',
-        'python_dateutil',
-        'PyYAML>=5.1',
-        'pyzmq<18.0.0',
-        'readline',
-        'requests',
-        'scipy',
-        'transitions',
-    ],
-    'social': [
-        'tweepy',
-    ],
-    'testing': [
-        'codecov',
-        'coverage',
-        'coveralls',
-        'mocket',
-        'pycodestyle==2.3.1',
-        'pytest>=3.6',
-        'pytest-cov',
-        'pytest-remotedata>=0.3.1'
-    ],
-}
+modules = [
+    'astroplan>=0.6',
+    'astropy>=4.0.0',
+    'dateparser',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'panoptes-utils>=0.2.10',
+    'photutils',
+    'pyserial>=3.1.1',
+    'python_dateutil',
+    'PyYAML>=5.1',
+    'pyzmq<18.0.0',
+    'readline',
+    'requests',
+    'scipy',
+    'transitions',
+    'tweepy',
+    'codecov',
+    'coverage',
+    'pycodestyle==2.3.1',
+    'pytest>=3.6',
+    'pytest-cov',
+    'pytest-remotedata>=0.3.1'
+]
 
 setup(name=PACKAGENAME,
       version=__version__,
@@ -77,30 +56,13 @@ setup(name=PACKAGENAME,
       author_email=AUTHOR_EMAIL,
       license=LICENSE,
       url=URL,
-      packages=find_packages(exclude=['tests', 'test_*']),
       keywords=KEYWORDS,
-      python_requires='>=3.6',
+      python_requires='>=3.7',
       setup_requires=['pytest-runner'],
-      tests_require=modules['testing'],
-      # List additional groups of dependencies here (e.g. development
-      # dependencies). You can install these using the following syntax,
-      # for example:
-      # $ pip install -e .[dev,test]
-      install_requires=modules['required'],
-      scripts=[
-          'bin/pocs',
-          'bin/pocs-shell',
-          'bin/peas-shell',
-      ],
-      extras_require={
-          'google': modules['google'],
-          'mongo': modules['mongo'],
-          'dev': modules['dev'],
-          'social': modules['social'],
-          'testing': modules['testing'],
-          'all': list(set(itertools.chain.from_iterable(modules.values())))
-      },
-      packages=find_namespace_packages(exclude=['tests', 'test_*']),
+      tests_require=modules,
+      install_requires=modules,
+      packages=find_namespace_packages(include=['pocs.*', 'peas.*'],
+                                       exclude=['tests', 'test_*']),
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
@@ -109,9 +71,7 @@ setup(name=PACKAGENAME,
           'Operating System :: POSIX',
           'Programming Language :: C',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3 :: Only',
           'Topic :: Scientific/Engineering :: Astronomy',
           'Topic :: Scientific/Engineering :: Physics',
